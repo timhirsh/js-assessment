d7539359b6299e3a5282a735514f7f60d99dda4b
Tim Hirsh <me@timhirsh.com>
2015-02-20 17:14:09 -0500

    Implement regex

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return (/\d/).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return (/([a-zA-Z])\1+/).test(str);
     },
 
     endsWithVowel : function(str) {
-
+      return (/[aeiou]$/i).test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      var match = str.match(/(\d{3})/) || [];
+      return match[0] || false;
     },
 
     matchesPattern : function(str) {
-
+      return (/^\d{3}-\d{3}-\d{4}$/).test(str);
     },
     isUSD : function(str) {
-
+      return (/^\$(\d{1,3})(,\d{3})*(\.\d{2})?$/).test(str);
     }
   };
 });
