4e13a360c141a49c0f5b53c884459db0326c73d9
Tim Hirsh <me@timhirsh.com>
2015-02-20 14:47:20 -0500

    Implement async

--- app/async.js
+++ app/async.js
@@ -1,13 +1,30 @@
+/*globals Promise:true*/
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+      var deferred = $.Deferred();
 
+      setTimeout(function() {
+        deferred.resolve(value);
+      }, 1);
+
+      return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
+      var deferred = $.Deferred();
 
+      $.getJSON(url).then(function(data) {
+        var people = $.map(data.people, function(person) {
+          return person.name;
+        });
+
+        deferred.resolve(people.sort());
+      });
+
+      return deferred.promise();
     }
   };
 });
