49fcb7832385f4c124cc9015a158ecedb9c58cc2
Tim Hirsh <me@timhirsh.com>
2015-02-20 15:40:29 -0500

    Implement objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+      var props = [];
 
+      for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          props.push(prop + ': ' + obj[prop]);
+        }
+      }
+
+      return props;
     }
   };
 });
