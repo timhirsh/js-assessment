abab73b7ba806b819b75682f5135f5ee971c7f59
Tim Hirsh <me@timhirsh.com>
2015-11-27 11:45:30 -0500

    Implement count

--- app/count.js
+++ app/count.js
@@ -3,6 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timeout;
+
+      function delayedLog(num) {
+        var delay = (num - start + 1) * 100;
+        timeout = setTimeout(function() {
+          console.log(num);
+        }, delay);
+      }
+
+      for (var i = start; i <= end; i++) {
+        delayedLog(i);
+      }
+
+      return {
+        cancel: function() {
+          clearTimeout(timeout);
+        }
+      };
     }
   };
 });
